3628c835b929f3ac67734a4be0e4b6242ba662fc
Roman Parashchenko <romikoops1@gmail.com>
2015-01-11 01:36:20 +0100

    Added authentication

--- demo_www/app/controllers/api/v1/base_controller.rb
+++ demo_www/app/controllers/api/v1/base_controller.rb
@@ -1,7 +1,10 @@
 require "application_responder"
 
 class API::V1::BaseController < ActionController::Base
+  protect_from_forgery with: :null_session
   self.responder = ApplicationResponder
+
+  before_filter :restrict_access
 
   private
 
@@ -9,6 +12,12 @@ class API::V1::BaseController < ActionController::Base
     {
         root: false
     }
+  end
+
+  def restrict_access
+    authenticate_or_request_with_http_token do |token, options|
+      token == ENV['HOWITZER_TOKEN']
+    end
   end
 
 end
