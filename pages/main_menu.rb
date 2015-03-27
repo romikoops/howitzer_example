b7a26397c1149d9172fde7b371313f2e047d62fe
Irish <irchik202@gmail.com>
2015-03-27 02:58:19 +0200

    changes for phantomjs

--- pages/main_menu.rb
+++ pages/main_menu.rb
@@ -3,9 +3,14 @@ module MainMenu
     base.class_eval do
       add_locator   :menu, '#main_menu'
       add_locator   :menu_item,  lambda{|name| {xpath: ".//*[@id='main_menu']//li[.='#{name}']/a"} }
+      add_locator :menu_small, '.navbar-toggle'
 
       def self.authenticated?
-          find(locator :menu)
+        if phantomjs_driver?
+          find(locator(:menu_small)).click
+        end
+
+        find(locator :menu)
         !first(locator(:menu_item).('Logout')).nil?
       end
     end
@@ -13,7 +18,11 @@ module MainMenu
 
   def choose_menu(text)
     log.info "Open '#{text}' menu"
-
-    find(locator(:menu_item).(text)).click
+    if phantomjs_driver?
+      find(locator(:menu_small)).click
+      click_link("#{text}",match: :first)
+    else
+      find(locator(:menu_item).(text)).click
+    end
   end
 end
