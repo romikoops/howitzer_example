12fc6f189fb14d81ef390c6afa24abdbec32404f
pavel-temtsunik <pavel.hvoya@gmail.com>
2015-04-06 00:28:34 +0300

    Corrected user_signs_up method

--- spec/bvt/functionality/account/cancelling_spec.rb
+++ spec/bvt/functionality/account/cancelling_spec.rb
@@ -1,12 +1,13 @@
 require 'spec_helper'
 
 feature "Account cancelling" do
-  scenario "Visitor can cancel his own account if he is signed in" do
-    user_signs_up
-    user_logs_in_into_system
+  scenario "User can cancel his own account if he is signed in" do
+    user_signs_up (Gen::given_user_by_number(0))
+    user_logs_in_into_system (Gen::given_user_by_number(0))
+
     EditAccountPage.open
-                   .given.cancel_my_account
-                   .accept_alert
+                   .cancel_my_account
+    accept_alert
     expect(HomePage.given.text).to include("Bye! Your account was successfully cancelled. We hope to see you again soon.")
   end
 
--- spec/sign_up_helper.rb
+++ spec/sign_up_helper.rb
@@ -1,6 +1,5 @@
 module SignUpHelper
-  def user_signs_up
-    user = Gen::given_user_by_number(0)
+  def user_signs_up(user)
     SignUpPage.
         open.fill_form(
         user_name: user.full_name,
@@ -8,10 +7,6 @@ module SignUpHelper
         password: user.password,
         password_confirmation: user.password)
         .submit_form
-    expect(HomePage).to_not be_authenticated
-
-    confirmation_text = "A message with a confirmation link has been sent to your email address. Please open the link to activate your account."
-    expect(HomePage.given.text).to include(confirmation_text)
 
     ConfirmationInstructionEmail.
         find_by_recipient(user.email).
@@ -21,8 +16,7 @@ module SignUpHelper
 
   end
 
-  def user_logs_in_into_system
-    user = Gen::given_user_by_number(0)
+  def user_logs_in_into_system(user)
     LoginPage.
         open.
         fill_form(email: user.email, password: user.password).
