824a9d1376d42b2e18f9b24e129b92ba0512dba3
Irish <irchik202@gmail.com>
2015-04-18 15:02:01 +0300

    viewing feature rspec added

--- spec/bvt/functionality/users/viewing_spec.rb
+++ spec/bvt/functionality/users/viewing_spec.rb
@@ -7,7 +7,6 @@ feature "Viewing Users" do
     sign_up_as(Gen::given_user_by_number(1))
     log_in_as(Gen::given_user_by_number(1))
     UsersPage.open.click_link user.email
-
     expect(UserViewPage.given.text).to include(user.email)
   end
 end
