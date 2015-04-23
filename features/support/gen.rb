7152aa15cb660e37e3e221adc603344db00c871e
pavel-temtsunik <pavel.hvoya@gmail.com>
2015-04-24 00:56:53 +0300

    Spaces normalization #2

--- features/support/gen.rb
+++ features/support/gen.rb
@@ -1,39 +1,40 @@
 module Gen
   class << self
 
- def article(params={})
-    prefix = serial
-    default = {
-        title: gen_article_title(prefix),
-        text: gen_article_text(prefix),
-    }
-    Article.new(default.merge(params))
- end
+    def article(params={})
+      prefix = serial
+      default = {
+          title: gen_article_title(prefix),
+          text: gen_article_text(prefix),
+      }
+      Article.new(default.merge(params))
+    end
 
- def gen_article_title(serial=nil)
-    gen_entity('article_title', serial)
- end
+    def gen_article_title(serial=nil)
+      gen_entity('article_title', serial)
+    end
 
- def gen_article_text(serial=nil)
-    gen_entity('article_text', serial*7)
- end
+    def gen_article_text(serial=nil)
+      gen_entity('article_text', serial*7)
+    end
 
- class Article
-   attr_reader :title, :text
-   def initialize(params={})
-     @title = params.delete(:title)
-     @text = params.delete(:text)
-   end
- end
+    class Article
+      attr_reader :title, :text
 
- def given_article_by_number(num)
-   data = DataStorage.extract('article', num.to_i)
-   unless data
-     data = Gen::article
-     DataStorage.store('article', num.to_i, data)
-   end
-   data
- end
+      def initialize(params={})
+        @title = params.delete(:title)
+        @text = params.delete(:text)
+      end
+    end
 
- end
+    def given_article_by_number(num)
+      data = DataStorage.extract('article', num.to_i)
+      unless data
+        data = Gen::article
+        DataStorage.store('article', num.to_i, data)
+      end
+      data
+    end
+
+  end
 end
