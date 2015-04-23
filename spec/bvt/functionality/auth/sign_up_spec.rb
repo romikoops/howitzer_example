a65da795bfac5426a2ba37697217d97f8f833fb1
Roman Parashchenko <romikoops1@gmail.com>
2015-04-23 02:39:56 +0200

    Replaced mailgun domain

--- spec/bvt/functionality/auth/sign_up_spec.rb
+++ spec/bvt/functionality/auth/sign_up_spec.rb
@@ -34,8 +34,7 @@ feature "Sign Up" do
     LoginPage.
         given.
         fill_form(email: user.email, password: user.password).submit_form
-    expect(HomePage).to be_authenticated
-    HomePage.wait_for_opened
+    expect(HomePage.given.text).to include('Signed in successfully.')
   end
 
   scenario "User can not sign up with blank data" do
@@ -46,7 +45,7 @@ feature "Sign Up" do
         password: nil,
         password_confirmation: nil).submit_form
     expect(HomePage).to_not be_authenticated
-    expect(SignUpPage.given.text).to include('2 errors prohibited this user from being saved: Email can't be blank Password can't be blank')
+    expect(SignUpPage.given.text).to include("2 errors prohibited this user from being saved: Email can't be blank Password can't be blank")
   end
 
   scenario "User can not sign up with blank username and password" do
@@ -58,7 +57,7 @@ feature "Sign Up" do
         password: nil,
         password_confirmation: nil).submit_form
     expect(HomePage).to_not be_authenticated
-    expect(SignUpPage.given.text).to include('1 error prohibited this user from being saved: Password can't be blank')
+    expect(SignUpPage.given.text).to include("1 error prohibited this user from being saved: Password can't be blank")
   end
 
   scenario "User can not sign up with blank email" do
@@ -70,7 +69,7 @@ feature "Sign Up" do
         password: user.password,
         password_confirmation: user.password).submit_form
     expect(HomePage).to_not be_authenticated
-    expect(SignUpPage.given.text).to include('1 error prohibited this user from being saved: Email can't be blank')
+    expect(SignUpPage.given.text).to include("1 error prohibited this user from being saved: Email can't be blank")
   end
 
   scenario "User can not sign up with invalid email and empty password" do
@@ -81,7 +80,7 @@ feature "Sign Up" do
         password: nil,
         password_confirmation: nil).submit_form
     expect(HomePage).to_not be_authenticated
-    expect(SignUpPage.given.text).to include('2 errors prohibited this user from being saved: Email is invalid Password can't be blank')
+    expect(SignUpPage.given.text).to include("2 errors prohibited this user from being saved: Email is invalid Password can't be blank")
   end
 
   scenario "User can not sign up with too short password" do
@@ -93,7 +92,7 @@ feature "Sign Up" do
         password: '1234567',
         password_confirmation: '1234567').submit_form
     expect(HomePage).to_not be_authenticated
-    expect(SignUpPage.given.text).to include('1 error prohibited this user from being saved: Password is too short (minimum is 8 characters)')
+    expect(SignUpPage.given.text).to include("1 error prohibited this user from being saved: Password is too short (minimum is 8 characters)")
   end
   scenario "User can not sign up when password confirmation doesn`t match" do
     user = Gen.user
@@ -104,11 +103,11 @@ feature "Sign Up" do
         password: '1234567890',
         password_confirmation: '1234567890123').submit_form
     expect(HomePage).to_not be_authenticated
-    expect(SignUpPage.given.text).to include('1 error prohibited this user from being saved: Password confirmation doesn't match Password')
+    expect(SignUpPage.given.text).to include("1 error prohibited this user from being saved: Password confirmation doesn't match Password")
   end
 
   scenario "User cannot sign up with duplicated email" do
-    sign_uped_as(Gen::given_user_by_number(0))
+    sign_up_as(Gen::given_user_by_number(0))
     user = Gen::given_user_by_number(0)
     SignUpPage.
         open.fill_form(
