03b8cb3e12b2ce47d1d390b95ff98bb6ed8d38a3
Roman Parashchenko <romikoops1@gmail.com>
2015-05-09 23:12:09 +0200

    fixed hardcoded url for capybara settings

--- boot.rb
+++ boot.rb
@@ -6,7 +6,7 @@ end
 
 require 'active_support/all' #TODO remove after upgrading to next version of Howitzer
 require 'howitzer'
-Capybara.asset_host = 'http://localhost:3000'
+Capybara.asset_host = app_url
 
 Dir[File.join(File.dirname(__FILE__), "./emails/**/*.rb")].each {|f| require f}
 Dir[File.join(File.dirname(__FILE__), "./pages/**/*.rb")].each {|f| require f}
