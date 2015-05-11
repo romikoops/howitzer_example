428d318bb7b7e1cf4ef8d9f2cb7c1842b96c3f52
SirNicholas <zozulyak.nick@gmail.com>
2015-05-11 22:38:18 +0300

    remove Capybara alert usage

--- spec/support/cancel_user_helper.rb
+++ spec/support/cancel_user_helper.rb
@@ -2,7 +2,6 @@ module CancelUserHelper
   def cancel_account()
     EditAccountPage.open
         .cancel_my_account
-    accept_alert
     expect(HomePage.given.text).to include("Bye! Your account was successfully cancelled. We hope to see you again soon.")
   end
 end
